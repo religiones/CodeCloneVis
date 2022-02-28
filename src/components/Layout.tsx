566a45dfc126d5fda1267114f75fe5bf589ade69
Kronos <1963024305@qq.com>
2022-02-28 14:30:42 +0800

    add goldenLayout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,15 +1,58 @@
-import React, {Component} from 'react';
+import GoldenLayout from "golden-layout";
+import 'golden-layout/src/css/goldenlayout-base.css'
+import 'golden-layout/src/css/goldenlayout-dark-theme.css'
+// components
+import Panel from './panel';
+
+
 import './css/layout.css'
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+window.React = React;
+window.ReactDOM = ReactDOM;
+const $ = require("jquery")
+
 
 interface layoutState{
   name: string,
   id: number
 }
 
-class Layout extends Component<any, layoutState> {
+const config: any = {
+  content: [
+    {
+      width: 100,
+      type: "column",
+      content: [
+        {
+            height: 20,
+            type: 'react-component',
+            componentName: 'Panel',
+            component: 'Panel',
+            isClosable: false,
+        },
+        {
+            height: 50,
+            type: 'react-component',
+            componentName: 'Panel',
+            component: 'Panel',
+            isClosable: false,
+        },
+        {
+            height: 30,
+            type: 'react-component',
+            componentName: 'Panel',
+            component: 'Panel',
+            isClosable: false,
+        },
+      ],
+    }
+  ]
+};
 
+class Layout extends Component<any, layoutState> {
   constructor(props: any){
-    super(props)
+    super(props);
     this.state = {
       name : "hello",
       id: 1
@@ -18,17 +61,16 @@ class Layout extends Component<any, layoutState> {
 
   render(){
       return (
-        <div className='container'>
-          <div>{this.state.name}</div>
-          <div>{this.state.id}</div>
-        </div>
+        <div id="container" className="container"></div>
       )
   }
 
   componentDidMount(){
-    // this.setState({
-    //   name: "world"
-    // })
+    setTimeout(()=>{
+      const layout = new GoldenLayout(config, $("#container"));
+      layout.registerComponent('Panel',Panel)
+      layout.init()
+    }, 0)
   }
 
 }
---
+++ src/components/panel.tsx
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+class panel extends Component<any, any> {
+  constructor(props: any){
+    super(props);
+  }
+
+  render(){
+      return (
+        <div>
+            2
+        </div>
+      )
+  }
+
+  componentDidMount(){
+
+  }
+}
+
+export default panel
